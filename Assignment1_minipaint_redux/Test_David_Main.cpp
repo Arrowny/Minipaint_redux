3777c30b8f1144c25c65c33ba6583d8fb64eddf7
David Klaus <davidklaus0@gmail.com>
2014-05-27 11:48:27 -0400

    Added GLUT headers to basic setup

---
+++ Assignment1_minipaint_redux/Test_David_Main.cpp
@@ -0,0 +1,12 @@
+#ifdef __APPLE__
+    #include "TargetConditionals.h"
+    #ifdef TARGET_OS_MAC
+        #include <GLUT/glut.h>
+        #include <OpenGL/OpenGL.h>
+    #endif
+#elif defined _WIN32 || defined _WIN64
+    #include <windows.h>
+	#include <GL/gl.h>
+	#include <GL/glut.h>
+	#include <GL/glu.h>
+#endif
