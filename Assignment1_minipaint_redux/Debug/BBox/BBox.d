a160b76d613c071e66a7415aada327b4f3f74160
David <davidklaus0@gmail.com>
2014-05-29 14:46:13 -0400

    updated for windows

--- Assignment1_minipaint_redux/Debug/BBox/BBox.d
+++
@@ -1,6 +0,0 @@
-BBox/BBox.d: ../BBox/BBox.cpp ../BBox/BBox.h \
- C:\Users\Joe\Documents\GitHub\Minipaint_redux\Assignment1_minipaint_redux\Misc/Misc.h
-
-../BBox/BBox.h:
-
-C:\Users\Joe\Documents\GitHub\Minipaint_redux\Assignment1_minipaint_redux\Misc/Misc.h:
